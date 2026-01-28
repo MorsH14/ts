279a9e79172002f7ae40adb1562b01fdb620edc8
MorsH14 <midemorsh@gmail.com>
2026-01-28 15:14:09 +0100

    created a todo, just something mini

--- todo.js
+++ todo.js
@@ -1,10 +1,38 @@
-let tasks = [
-  { task: "eat", done: false },
-  { task: "sleep", done: true },
-];
+let tasks = [];
 
-for (let i = 0; i < tasks.length; i++) {
-  console.log(
-    `${i + 1}. ${tasks[i].task} - ${tasks[i].done ? "Done" : "Not done"}`,
-  );
+function addTask(newTask) {
+  tasks.push({ task: "sleep", done: false });
 }
+
+function markDone(index) {
+  if (tasks[index]) {
+    tasks[index].done = true;
+    console.log("Task found");
+  } else {
+    console.log("Task not found");
+  }
+}
+
+function deleteTask(index) {
+  if (tasks[index]) {
+    tasks.splice(index, 1);
+  } else console.log("Task not found");
+}
+
+function showTasks() {
+  for (let i = 0; i < tasks.length; i++) {
+    console.log(
+      `${i + 1}. ${tasks[i].task} - ${tasks[i].done ? "✅ Done" : "❌ Not done"}`,
+    );
+  }
+}
+
+addTask("Buy milk");
+addTask("Do homework");
+addTask("Learn JS");
+
+markDone(1);
+
+deleteTask(0);
+
+showTasks();
