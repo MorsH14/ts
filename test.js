d35d0a3e0222d50588436b106bb31e7409316f7d
MorsH14 <midemorsh@gmail.com>
2026-01-28 14:31:50 +0100

    worked on analysing sentence using js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-const sentence = "I am learning javascript every night";
-const words = sentence.split(" ");
+const mySentence = "I am learning javascript every night";
+const words = mySentence.split(" ");
 
 let shortestWord = words[3];
 
@@ -11,3 +11,52 @@ for (let i = 0; i < words.length; i++) {
 }
 
 console.log(shortestWord);
+
+const sentence = "javascript is fun and javascript is powerful";
+const word = sentence.toLowerCase().split(" ");
+
+let wordCount = {};
+
+for (let i = 0; i < word.length; i++) {
+  if (wordCount[word[i]]) {
+    wordCount[word[i]] += 1;
+  } else {
+    wordCount[word[i]] = 1;
+  }
+}
+
+console.log(wordCount);
+
+function analyzeSentence(sentence) {
+  const words = sentence.toLowerCase().split(" ");
+
+  let wordCount = {};
+  let longestWord = words[0];
+  let shortestWord = words[0];
+
+  for (let i = 0; i < words.length; i++) {
+    // count words
+    if (wordCount[words[i]]) {
+      wordCount[words[i]]++;
+    } else {
+      wordCount[words[i]] = 1;
+    }
+
+    // longest word
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+
+    // shortest word
+    if (words[i].length < shortestWord.length) {
+      shortestWord = words[i];
+    }
+  }
+
+  return {
+    wordCount: wordCount,
+    longestWord: longestWord,
+    shortestWord: shortestWord,
+    totalWords: words.length,
+  };
+}
